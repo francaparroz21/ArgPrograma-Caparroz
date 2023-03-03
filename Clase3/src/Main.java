b0b0150b1781326efb082b8ef862ef134a7260bf
francaparroz21 <francaparroz21@gmail.com>
2023-03-02 23:54:58 -0300

    terminamos ejercicio 1 B de la clase 3

--- Clase3/src/Main.java
+++ Clase3/src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,8 +28,16 @@ public class Main {
         mismos y los retorne en un vector de 3
 
         sortNumbers
-
         */
+
+        //1,6,4,2,7,18,999
+        //expected: 1,2,4,6,7,18,999
+        int[] numbers = new int[]{1,6,4,2,7,18,999};
+        System.out.println(Arrays.toString(sortNumbers(numbers)));
+
+        /*
+
+         */
     }
         public static int getRepeatedLetterCount(String text, char letter){
             int count = 0;
@@ -39,7 +49,16 @@ public class Main {
             return count;
         }
 
-        public static List sortNumbers(List numbers){
+        public static int[] sortNumbers(int[] numbers){
+            int aux;
+            for(int i = 0; i < numbers.length-1; i++)
+                for(int j=0; j < numbers.length-i-1; j++)
+                    //Segun como lo quisieramos ordenar, cambiariamos el < por > y viceversa.
+                    if(numbers[j+1] < numbers[j]){
+                        aux = numbers[j+1];
+                        numbers[j+1] = numbers[j];
+                        numbers[j] = aux;
+                    }
             return numbers;
         }
 }
