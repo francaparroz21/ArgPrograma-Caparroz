69d74ed7c5b2c41c8f70218ea6bdbfeb5c895c08
francaparroz21 <francaparroz21@gmail.com>
2023-03-09 17:00:38 -0300

    listo el 1-b

--- Clase4/src/Main.java
+++ Clase4/src/Main.java
@@ -1,16 +1,38 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /*haga un main, donde por parámetro ponga 3 números y una letra que
+        represente ascendente o descendente y los muestre ordenados por tal criterio
         int[] numbers = new int[]{1, 99, 32, 2};
         System.out.println(Arrays.toString(sort(numbers, 'a')));
-        System.out.println(Arrays.toString(sort(numbers, 'd')));
+        System.out.println(Arrays.toString(sort(numbers, 'd')));*/
+
+        /*
+        haga lo mismo, pero solicitando los parámetros de a uno por consola
+         */
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Ingrese cuantos numeros quiere ordenar: ");
+        int nNumeros = scr.nextInt();
+        int[] numbers = new int[nNumeros];
+
+        for (int i = 0; i < nNumeros; i++) {
+            System.out.println("Ingrese un numero: ");
+            int n = scr.nextInt();
+            numbers[i] = n;
+        }
+
+        System.out.println("Ingrese 'a' para ordenar ascendente \n Ingrese 'd' para ordenar descendente");
+        String s = scr.next();
+
+        System.out.println(Arrays.toString(sort(numbers, s)));
 
     }
 
-    public static int[] sort(int[] numbers, char s) {
-        if (s == 'a') return sortAscendente(numbers);
-        else if (s == 'd') return sortDescendente(numbers);
+    public static int[] sort(int[] numbers, String s) {
+        if (s.equals("a")) return sortAscendente(numbers);
+        else if (s.equals("d")) return sortDescendente(numbers);
         return numbers;
     }
 
@@ -28,7 +50,7 @@ public class Main {
 
     public static int[] sortDescendente(int[] numbers) {
         int aux;
-        for (int i = 0; i < numbers.length - 1; i++)
+        for (int i = 0; i < numbers.length - 1 ; i++)
             for (int j = 0; j < numbers.length - 1; j++)
                 if (numbers[j + 1] > numbers[j]) {
                     aux = numbers[j + 1];
